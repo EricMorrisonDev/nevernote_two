b1f8d75b6e0e1e4a2746e75e6adff463b5c4433b
Eric Morrison <eric_m250@outlook.com>
2026-07-29 16:25:49 -0700

    added default createdAt to user schema in prisma, built out login route

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -2,11 +2,13 @@ import { NextRequest, NextResponse } from "next/server";
 import { signupSchema } from "@/app/lib/validation/auth";
 import { prisma } from "@/app/lib/prisma";
 import { hashPassword } from "@/app/lib/hash";
+import { cookies } from "next/headers";
 
 
+const SESSION_COOKIE_NAME = "session"
+const SESSION_MAX_AGE_DAYS = 30
 
-
-export async function signup(request: NextRequest) {
+export async function POST(request: NextRequest) {
 
     try {
         // validate request
@@ -14,13 +16,13 @@ export async function signup(request: NextRequest) {
     
         const validatedData = signupSchema.safeParse(raw)
     
-        if(!validatedData){
+        if(!validatedData.success){
             return NextResponse.json({
                 error: "Invalid login request"
             })
         }
 
-        const { email, password } = validatedData
+        const { email, password, userName } = validatedData.data
     
         // check if user already exists
     
@@ -37,17 +39,53 @@ export async function signup(request: NextRequest) {
         }
 
         // hash password
-        const passwordHash = hashPassword(validatedData.data?.password)
+        const hashResult = await hashPassword(password)
+
+        if(!hashResult.success){
+            return NextResponse.json({
+                error: "failed to generate password hash"
+            })
+        }
+
+        const passwordHash = hashResult.hash
         
         // run prisma query to create user
 
         const user = await prisma.user.create({
             data: {
-                email: validatedData.data?.email
+                userName,
+                email,
+                passwordHash
             }
         })
 
-    } catch (err) {
+        if(!user){
+            return NextResponse.json({
+                error: "Failed to create user"
+            })
+        }
 
+        // create an expiresAt value by adding max age days value above to today's date
+        const now = new Date()
+        const expiresAt = now.setDate(now.getDate() + SESSION_MAX_AGE_DAYS)
+
+        // run prisma query saving session with userId and expiresAt stored in data obj
+
+        const session = prisma.session.create({
+            data: {
+                userId: user.id
+            }
+        })
+
+        if(!session){
+            return NextResponse.json({
+                error: "Failed to create session"
+            })
+        }
+
+
+
+    } catch (err) {
+        console.error(err)
     }
 }
--- app/lib/hash.ts
+++ app/lib/hash.ts
@@ -2,9 +2,14 @@ import bcrypt from "bcrypt"
 
 const saltRounds = 10
 
-export async function hashPassword(password: string): Promise<string> {
+interface hashResult {
+    hash: string,
+    success: boolean
+}
+
+export async function hashPassword(password: string): Promise<hashResult> {
     
     const hashedPassword = await bcrypt.hash(password, saltRounds)
 
-    return hashedPassword
+    return { hash: hashedPassword, success: true }
 }
